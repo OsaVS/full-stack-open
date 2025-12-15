f418a4882fc7000e66984dcb8f806088d1d2ff71
OsaVS <41975253+OsaVS@users.noreply.github.com>
2025-12-15 07:42:41 +0530

    unicafe-1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -23,12 +23,14 @@ const Statistics = (props) => {
   } else {
     return (
     <table>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="average" value={props.average} />
-      <StatisticLine text="positive" value={props.positive + " %"} />
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive + " %"} />
+      </tbody>
     </table>
     )
   }
