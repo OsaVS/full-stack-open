d69358a515995e2156750a9c9029032264c247a3
OsaVS <41975253+OsaVS@users.noreply.github.com>
2025-12-17 07:01:42 +0530

    phonebook-

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault()
+
+    setPersons(persons.concat({
+      name: newName
+    }))
+
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNameSubmit}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(
+          person => (
+            <p key={person.name}>{person.name}</p>
+          )
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
