b97432fabe4a245b2a2ecd786b10e0be07e01642
OsaVS <41975253+OsaVS@users.noreply.github.com>
2025-12-17 06:26:47 +0530

    courseinfo-2.10

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -3,11 +3,17 @@ import Header from './Header'
 
 const Course = ({course}) => {
 
+    const total = course.parts.reduce(
+        (sum, part) => sum + part.exercises, 0
+    )
+
     return(
         <div>
             <Header name={course.name} />
 
             <Content parts={course.parts} />
+
+            <p><b>total of {total} exercises</b></p>
         </div>
     )
     
