87eaaebea6472691e5f884c1e27cecd78b7efcd1
OsaVS <41975253+OsaVS@users.noreply.github.com>
2026-01-15 07:02:31 +0530

    phonebook-3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,6 +33,16 @@ app.get('/api/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+})
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
