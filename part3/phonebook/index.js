f1bff3f1bdb642d55f26a5514bdfadc4d628dfb6
OsaVS <41975253+OsaVS@users.noreply.github.com>
2026-01-20 06:27:33 +0530

    port modification

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -91,7 +91,7 @@ const generateId = () => {
   return Math.floor(Math.random() * 1000);
 }
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
