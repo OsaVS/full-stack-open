6b317af00d5aa8815576a25c8de09e6c60b64e99
OsaVS <41975253+OsaVS@users.noreply.github.com>
2026-01-27 05:46:32 +0530

    phonebook-3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -20,38 +20,53 @@ app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons);
   })
+  .catch(error => {
+    next(error)
+  })
 });
 
 app.get('/api/info', (req, res) => {
   Person.find({}).then(persons => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
   })
+  .catch(error => {
+    next(error)
+  })
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   const id = req.params.id;
   const person = Person.findById(id)
   .then(person => {
-    console.log(person)
-    res.json(person);
+    if (person) {
+      console.log(person)
+      res.json(person);
+    } else {
+      res.status(404).send({error: 'Person not found'})
+    }
+    
   })
   .catch(error => {
-    res.status(404).end()
+    next(error)
   })
   
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   const id = req.params.id;
   Person.findByIdAndDelete(id)
         .then(result => {
             res.status(204).end();
-        })  
+        }) 
+        .catch(error => {
+          console.log(error)
+          next(error)
+        }) 
 })
 
 
 
-app.post('/api/persons',(req, res) => {
+app.post('/api/persons',(req, res, next) => {
   const body = req.body
   if (!body.name || !body.number) {
     return (res.status(400).json({
@@ -66,43 +81,62 @@ app.post('/api/persons',(req, res) => {
                 error:"name must be unique"
               }))
           }
+          const person = new Person({
+            name: body.name,
+            number: body.number,
+          })
+
+          person.save()
+            .then(result => {
+              console.log('person added successfully')
+              res.json(result)
+            })
         })
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-
-  person.save()
-        .then(result => {
-          console.log('person added successfully')
-          res.json(result)
+        .catch(error => {
+          next(error)
         })
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body
   if (!body.name || !body.number) {
     return (res.status(400).json({
       error:"Content Missing"
     }))
   }
-  Person.findByIdAndUpdate(req.params.id, {
-      name: body.name,
-      number: body.number, 
-    },
-    { new: true, runValidators: true })
-        .then(result => {
-          res.json(result)
+  Person.findById(req.params.id)
+        .then(person => {
+          if(!person){
+            res.status(404).end()
+          }
+          person.number = body.number
+
+          person.save().then(result => {
+            res.json(result)
+          })
         })
         .catch(error => {
-          res.status(400).end()
+          next(error)
         })
 })
 
-// const generateId = () => {
-//   return Math.floor(Math.random() * 1000);
-// }
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'Unknown Endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const port = process.env.PORT;
 app.listen(port, () => {
